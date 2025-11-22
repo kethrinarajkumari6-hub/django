9c6c564256f88fddb58a35ce6c89a00073c613f0
kethrinarajkumari6-hub <kethrinarajkumari6@gmail.com>
2025-11-22 13:18:02 +0530

    hi

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function vote(){
-    var name= document.getElementById("name").value;
-    var age= document.getElementById("age").value;
-    var res= document.getElementById("res");
+function vote() {
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var res = document.getElementById("res");
 
-    if(age>=18){
-        res.innerHTML= "You are Eligible";
-    }
-    else{
-        res.innerHTML= "You are still a child";
+    if (age >= 18) {
+        res.innerHTML= "you are eligible to vote.";
+    } else {
+        res.innerHTML = "you are still a child.";
     }
 }
